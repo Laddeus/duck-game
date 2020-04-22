8996d541baf7a8c6e59ce7e1d35452e85fafc7c3
Laddeus <33806862+Laddeus@users.noreply.github.com>
2020-04-22 21:11:40 +0300

    matrix test

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-let canvas = document.querySelector('canvas');
-let context = canvas.getContext('2d');
+// classes
+class Square{
+    constructor() {
+        this.width = 25;
+        this.height = 25;
+        this.object = undefined;
+    }
+}
 
-let squareSize = 25;
-let canvasGrid = [][];
+class GameGrid{
+    constructor() {
+        this.canvasGrid = new Array(800 / 25);
+
+        for (let i = 0; i < 800 / 25 ; i++) {
+            this.canvasGrid[i] = new Array(600 / 25 );
+            for (let j = 0; j < 600 / 25 ; j++) {
+                this.canvasGrid[i][j] = new Square();
+            }
+        }
+    }
+
+    getSquare(x, y){
+        return this.canvasGrid[Math.round(y/25)][Math.round(x/25)];
+    }
+}
+
+// functions
+
+
+
+
+
+// main
+/*let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');*/
+
+let squareSize = 25; // 25x25
+let gameGrid = new GameGrid();
+let someSquare = gameGrid.getSquare(42, 456);
+
+for (let i = 0; i < gameGrid.canvasGrid.length; i++) {
+    let index = gameGrid.canvasGrid[i].indexOf(someSquare);
+
+    if(index != -1){
+        console.log("found index" + index);
+    }
+}
+
