92cfb25110ddf0cf887dff68bda3ec027822651e
Laddeus <33806862+Laddeus@users.noreply.github.com>
2020-04-22 21:11:40 +0300

    matrix test

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let canvas = document.querySelector('canvas');
-let context = canvas.getContext('2d');
+/*let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');*/
 
-let squareSize = 25;
-let canvasGrid = [][];
+let squareSize = 25; // 25x25
+let canvasGrid = [];
+
+for (let i = 0; i < 800 / 25 ; i++) {
+    canvasGrid.push(new Array(600 / 25 ));
+    for (let j = 0; j < 600 / 25; j++) {
+        canvasGrid[i][j] = "test";
+    }
+}
+
+for (let i = 0; i < canvasGrid.length; i++) {
+    console.log(canvasGrid[i].toString());
+}
